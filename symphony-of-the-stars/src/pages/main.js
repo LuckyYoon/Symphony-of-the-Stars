08b47aa30c0ff3f8fa101c6862b5591a2dc382bd
LuckyYoon <justinyoon1234@gmail.com>
2024-10-05 18:31:29 +0900

    Added hamburger icon

--- symphony-of-the-stars/src/pages/main.js
+++ symphony-of-the-stars/src/pages/main.js
@@ -2,14 +2,31 @@ import React, { useState } from "react";
 import 
 {
   Box,
-  Text
+  Text,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  Flex,
+  IconButton,
+  useDisclosure
 } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export default function MainPage()
 {
+    const {
+        isOpen: isDrawerOpen,
+        onOpen: onDrawerOpen,
+        onClose: onDrawerClose,
+    } = useDisclosure();
+
+    const btnRef = React.useRef();
+
     return (
         <Box
-        bgImage="url('/assets/background/53567451213_74765a4a5a_4k.jpg')"
+        bgImage="url('/assets/image/background/53567451213_74765a4a5a_4k.jpg')"
         bgSize="cover"
         bgPosition="center"
         bgBlendMode="darken"
@@ -47,6 +64,81 @@ export default function MainPage()
             filter="blur(25px)"
             />
             </Box>
+            <IconButton
+            icon={<HamburgerIcon />}
+            ref={btnRef}
+            onClick={onDrawerOpen} // Use drawer's onOpen
+            position="absolute"
+            top="3%"
+            right="3%"
+            bg="accent.600"
+            padding="0"
+            fontSize="32px"
+            color="accent.300"
+            _hover={{ bg: "accent.600", color: "accent.400" }}
+            />
+            {/* Side Panel */}
+            <Drawer
+                isOpen={isDrawerOpen} // Use drawer's isOpen
+                placement="right"
+                onClose={onDrawerClose} // Use drawer's onClose
+                finalFocusRef={btnRef}
+                size="xs"
+            >
+                <DrawerOverlay />
+                <DrawerContent bg="accent.100" height="100%">
+                <DrawerBody>
+                    <Flex
+                    height="100%"
+                    alignItems="center"
+                    justifyContent="center"
+                    flexDirection="column"
+                    gap={20}
+                    >
+                    <Button
+                        bg="accent.100"
+                        color="accent.500"
+                        fontSize="3xl"
+                        _hover={{ bg: "accent.100", color: "accent.300" }}
+                    >
+                        <Text>ABOUT US</Text>
+                    </Button>
+                    <Button
+                        bg="accent.100"
+                        color="accent.500"
+                        fontSize="3xl"
+                        _hover={{ bg: "accent.100", color: "accent.300" }}
+                    >
+                        <Text>PRESENTATION</Text>
+                    </Button>
+                    <Button
+                        bg="accent.100"
+                        color="accent.500"
+                        fontSize="3xl"
+                        _hover={{ bg: "accent.100", color: "accent.300" }}
+                    >
+                        <Text>GITHUB</Text>
+                    </Button>
+                    <Button
+                        bg="accent.100"
+                        color="accent.500"
+                        fontSize="3xl"
+                        _hover={{ bg: "accent.100", color: "accent.300" }}
+                    >
+                        <Text>NOTION</Text>
+                    </Button>
+                    <Button
+                        bg="accent.100"
+                        color="accent.500"
+                        fontSize="3xl"
+                        _hover={{ bg: "accent.100", color: "accent.300" }}
+                    >
+                        <Text>WORKS CITED</Text>
+                    </Button>
+                    </Flex>
+                </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </Box>
     );
 }
